f613ad678f3cac0dadf99857025bd17932b24396
Mike Lysikov <d8corp@mail.ru>
2026-02-25 23:15:49 +0300

    Add result table

--- src/describe.ts
+++ src/describe.ts
@@ -3,6 +3,22 @@ import getDeep from './getDeep'
 import chalk from 'chalk'
 import {beautifyNumber} from './test'
 
+function getProgressColor (progress: number, str: string) {
+  if (progress > 15) {
+    return chalk.green(str)
+  }
+
+  if (progress > 10) {
+    return chalk.cyan(str)
+  }
+
+  if (progress > 5) {
+    return chalk.yellow(str)
+  }
+
+  return chalk.red(str)
+}
+
 function describe (name: string, callback: () => any, timeout = scope.currentTimeout) {
   const beforeTimeout = scope.currentTimeout
   scope.currentTimeout = timeout
@@ -23,6 +39,24 @@ function describe (name: string, callback: () => any, timeout = scope.currentTim
   }
   if (winner.value) {
     console.log(`${getDeep()}╘ ${chalk.white(name + ': ')}${chalk.cyan(winner.name)} [${chalk.cyan(beautifyNumber(winner.value))}]`)
+
+    const names = Object.keys(result).sort((a, b) => result[b].value - result[a].value)
+    const values = names.map(name => String(beautifyNumber(result[name].value)))
+    const percentage = names.map(name => `${beautifyNumber(result[name].value / winner.value * 100, 2)}%`)
+    const progress = names.map(name => {
+      const progress = Math.round(result[name].value / winner.value * 20)
+      return getProgressColor(progress, '▰'.repeat(progress)) + '▱'.repeat(20 - progress)
+    })
+
+    const nameSize = Math.max(...names.map(name => name.length))
+    const valuesSize = Math.max(...values.map(value => value.length))
+    const percentageSize = Math.max(...percentage.map(percentage => percentage.length))
+
+    console.log(`${getDeep()}┌─${'─'.repeat(nameSize)}─┬─${'─'.repeat(valuesSize)}─┬─${'─'.repeat(20)}─┬─${'─'.repeat(percentageSize)}─┐`)
+    names.forEach((testName, index) => {
+      console.log(`${getDeep()}│ ${testName.padEnd(nameSize)} │ ${values[index].padEnd(valuesSize)} │ ${progress[index]} │ ${percentage[index].padEnd(percentageSize)} │`)
+    })
+    console.log(`${getDeep()}└─${'─'.repeat(nameSize)}─┴─${'─'.repeat(valuesSize)}─┴─${'─'.repeat(20)}─┴─${'─'.repeat(percentageSize)}─┘`)
   } else {
     console.log(getDeep() + '╘ ' + chalk.white(name))
   }
